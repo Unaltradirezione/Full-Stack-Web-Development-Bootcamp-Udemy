d6d3ebe79a8c1df934524ac631ce4cc2dba1575b
unaltradirezione <unaltradirezione@hotmail.com>
2025-06-08 16:37:10 -0600

    cambios a carpeta backend

--- 2 Backend/23 node.js/2.2+Native+Modules/2.2 Native Modules/solution.js
+++ 2 Backend/23 node.js/2.2+Native+Modules/2.2 Native Modules/solution.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
 
-fs.writeFile("message.txt", "Hello Node", (err) => {
-  if (err) throw err;
-  console.log("The file has been saved!");
-});
+// fs.writeFile("message.txt", "Hello Node", (err) => {
+//   if (err) throw err;
+//   console.log("The file has been saved!");
+// });
 
 fs.readFile("Hello from Victor!.txt", "utf8", (err, data) => {
   if (err) throw err;
